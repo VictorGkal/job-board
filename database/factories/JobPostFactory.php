34f3de2a43a99a25506ad6d4e91a0a8658f96044
Victor Galusca <victorgalouska@gmail.com>
2026-03-13 01:42:28 +0200

    Fix faker and firstOrCreate in seeders

--- database/factories/JobPostFactory.php
+++ database/factories/JobPostFactory.php
@@ -13,13 +13,13 @@ class JobPostFactory extends Factory
         return [
             'user_id'     => User::where('role', 'employer')->first()->id,
             'category_id' => Category::inRandomOrder()->first()->id,
-            'title'       => fake()->jobTitle(),
-            'description' => fake()->paragraphs(3, true),
-            'location'    => fake()->city(),
-            'type'        => fake()->randomElement(['full-time', 'part-time', 'freelance', 'internship']),
-            'salary_min'  => fake()->numberBetween(20000, 50000),
-            'salary_max'  => fake()->numberBetween(50000, 120000),
-            'status'      => fake()->randomElement(['open', 'closed']),
+            'title'       => $this->faker->jobTitle(),
+            'description' => $this->faker->paragraphs(3, true),
+            'location'    => $this->faker->city(),
+            'type'        => $this->faker->randomElement(['full-time', 'part-time', 'freelance', 'internship']),
+            'salary_min'  => $this->faker->numberBetween(20000, 50000),
+            'salary_max'  => $this->faker->numberBetween(50000, 120000),
+            'status'      => $this->faker->randomElement(['open', 'closed']),
         ];
     }
 }
